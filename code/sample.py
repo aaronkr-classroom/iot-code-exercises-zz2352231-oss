6c9d6d0886945072297c69fd25b4db41d2ec3eaa
zz2352231-oss <zz2352231@gmail.com>
2026-03-23 11:30:31 +0900

    Update sample.py

--- code/sample.py
+++ code/sample.py
@@ -92,3 +92,21 @@ print(f"s3: {sub_list_3}")
 print(f"s4: {sub_list_4}")
 print(f"s5: {sub_list_5}")
 
+#3_tuple.py
+#rgb색상을 표션하는 튜플 예제
+black = (0,0,0) #rgb
+white = (255,255,255)
+red = (255,0,0)
+green =(0,255,0)
+blue= (0,0,255)
+my_color = (24,75,189)
+
+#my_color[2] = 0 # blue 삭제
+my_color= (24,75,0)
+
+
+my_color= [
+    black, white, green, blue,my_color
+]
+for color in my_colors:
+    print(color)
