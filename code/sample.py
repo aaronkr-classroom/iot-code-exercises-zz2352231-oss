100fa21a49f60b661184301e19e9d716b456a9d4
zz2352231-oss <zz2352231@gmail.com>
2026-03-23 11:15:30 +0900

    Update sample.py

--- code/sample.py
+++ code/sample.py
@@ -28,3 +28,52 @@ def fibonacci_sequence(n):
 
 # Example
 fibonacci_sequence(10)
+
+#3_bool.py
+a = true 
+b= false
+c= true
+#숫자에서 boold로 변환
+d= bool(0)
+e=bool(1)
+f=bool(-1)
+g=bool(10)
+h=bool("1")
+i=bool("")
+j= bool('heelo')
+print(a,b,c,d,e,f,g,h,i,j)
+
+
+
+
+3_list.py
+#리스트 생성
+my_list = [10,20,30,40]
+#리스트 요소에 접근
+print(my_list[0])
+print(my_list[3])
+#리시트에 요소 추가
+my_list.append(50)
+
+#특정 위치에 요소 삽입
+my_list.insert(1,15)
+print(my_list)
+
+#리스트에서 요소 삭제
+my_list.remove(30)
+print(my_list)
+
+#인덱스로 요소 삭제
+delect my_list[2]
+print(my_list)
+
+#리스트 길이 구하기
+print(len(my_list))
+
+#리스트의 모든 요소 반복
+for item in my_list:
+print(item)
+
+#리스트 슬라이싱
+sub_list_1 = my_list[1:3]
+print(sub_list)
